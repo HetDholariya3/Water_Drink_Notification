a39714626d354a396634cdd7d0fff899cee57c09
HetDholariya3 <k.radhe3847@gmail.com>
2025-06-08 19:01:30 +0530

    water_drink_notification.py

--- water_drink_notification.py
+++ water_drink_notification.py
@@ -1,10 +1,35 @@
 import time
 from plyer import notification
+import logging
+from datetime import datetime
+import winsound 
 
-time_hour = float(input("Set the hours after you want to drink  water "))
+logging.basicConfig(filename="water_reminder_log.txt", level=logging.INFO, format="%(asctime)s - Reminder Triggered")
+
+user_input = input("Enter the reminder time (YYYY-MM-DD HH:MM): ").strip()
+
+try:
+    target_time = datetime.strptime(user_input, "%Y-%m-%d %H:%M")
+except ValueError:
+    print("Invalid format. Please enter time in 'YYYY-MM-DD HH:MM'")
+    exit()
+
+print(f"Reminder set for {target_time}. Waiting...")
+
 while True:
-    time.sleep(3600 * time_hour)
-    notification.notify(
-        title="Water", 
-        message="you should drink water",
-        timeout=2)
+    now = datetime.now()
+    if now >= target_time:
+        winsound.Beep(1000, 1000)
+
+        # Show notification
+        notification.notify(
+            title="Water Reminder",
+            message="Time to drink water!",
+            timeout=10
+        )
+
+        
+        logging.info("Water reminder triggered at specific time.")
+        break
+
+    time.sleep(10) 
